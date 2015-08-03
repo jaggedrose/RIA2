230c60739b4f94c8975b5c76df82e4b096acbf21
Jovan <jovan.lund@gmail.com>
2015-08-03 12:04:59 +0200

    included mongresto.js , required it and init in in app.js

--- app.js
+++ app.js
@@ -13,6 +13,9 @@ var bodyParser = require('body-parser');
 
 var app = express();
 
+var mongresto = require("./mongresto.js");
+mongresto.init(app[,options]);
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
