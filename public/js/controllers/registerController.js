4d951112cd27bbcffa586fb9c3d55d764c45fa9e
Sus1221 <susanne881221@hotmail.com>
2015-08-05 15:50:49 +0200

    validation for some more of the inputs

--- public/js/controllers/registerController.js
+++ public/js/controllers/registerController.js
@@ -1,17 +1,2 @@
-//"myAppName" controller.
-app.controller("registerController", ["$http", "$scope", "User", "Login", function($http, $scope, User, Login) {
-  $scope.newUser = {};
-  $scope.newUserCreate = function() {
-    $scope.createdUser = User.create($scope.newUser, function(data) {
-      console.log("User.create($scope.newUser)",data);
-      
-      if (data.status) {
-        // ERROR
-      } else {
-        // SUCCESS
-      }
-    });
-  };
-}]);
 
-
+  
