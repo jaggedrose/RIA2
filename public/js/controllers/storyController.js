44c748605d4561670ee0dacab6df990456ebbe3b
Electromotif <pjhk@yahoo.com>
2015-08-05 16:10:31 +0200

    working on storyController, updated uploadStory.html. We can now write a full story (3 sections in turn).

--- public/js/controllers/storyController.js
+++ public/js/controllers/storyController.js
@@ -1,33 +1,38 @@
 //"myAppName" controller.
-console.log("hej");
+
 app.controller("storyController", ["$http", "$scope", "Story", function($http, $scope, Story) {
 
 
 
   // Create a new story and save immediately to the db
-  window.a = $scope.myStory = Story.create({title:"Untitled"});
+  Story.create({title:"Untitled"},function(arrayOfNewStories){
+    $scope.storyData = arrayOfNewStories[0];
+  });
+  // Counter
+  var currentSection = 1;
 
   // A representation of the currently edited section
-  $scope.storySection = {sectionNo:1, header: "", text: "", image: ""};
-
+  $scope.storySection = {sectionNo:1, header: "", text: "", img: ""};
+    console.log($scope.storySection);
   // On section change
-  function onSectionForward(back){
+  $scope.onSectionForward = function(back){
+    console.log($scope);
     // Add the current section in the larger myStory object
-    $scope.myStory["section" + $scope.storySection.sectionNo] = $scope.storySection;
+    $scope.storyData["section" + currentSection] = $scope.storySection;
     // Save to DB
-    Story.update({_id:$scope.myStory._id},$scope.myStory);
+    Story.update({_id:$scope.storyData._id},$scope.storyData);
     // Don't do anything else if we are the last section
     if(
-      ($scope.storySection.sectionNo >= 3 && !back) ||
-      ($scope.storySection.sectionNo <= 1 && back)
+      (currentSection >= 3 && !back) ||
+      (currentSection <= 1 && back)
     ){return;}
     // Increment section number
-    $scope.storySection.sectionNo += (back ? -1 : 1);
+    currentSection += (back ? -1 : 1);
     // Now change to what is stored for this section in myStory
-    $scope.storySection =  $scope.myStory["section" + $scope.storySection.sectionNo];
+    $scope.storySection =  $scope.storyData["section" + currentSection] || {};
   }
 
-  function onSectionBack(){
+  $scope.onSectionBack = function(){
     onSectionForward(true);
   }
 
