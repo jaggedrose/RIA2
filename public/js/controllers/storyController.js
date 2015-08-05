2194404b3c5208a2af342ce83b0733e4e28a9cc5
Electromotif <pjhk@yahoo.com>
2015-08-05 15:28:55 +0200

    work on storyController, included storyController in index.html

--- public/js/controllers/storyController.js
+++ public/js/controllers/storyController.js
@@ -1,11 +1,11 @@
 //"myAppName" controller.
-app.controller("StoryController", ["$http", "$scope", "Story", function($http, $scope, Story) {
-  $scope.storysection = 1;   
-  
+console.log("hej");
+app.controller("storyController", ["$http", "$scope", "Story", function($http, $scope, Story) {
+
 
 
   // Create a new story and save immediately to the db
-  $scope.myStory = Story.create({title:"Untitled"});
+  window.a = $scope.myStory = Story.create({title:"Untitled"});
 
   // A representation of the currently edited section
   $scope.storySection = {sectionNo:1, header: "", text: "", image: ""};
@@ -19,58 +19,17 @@ app.controller("StoryController", ["$http", "$scope", "Story", function($http, $
     // Don't do anything else if we are the last section
     if(
       ($scope.storySection.sectionNo >= 3 && !back) ||
-      $scope.storySection.sectionNo <= 1 && back)
+      ($scope.storySection.sectionNo <= 1 && back)
     ){return;}
     // Increment section number
     $scope.storySection.sectionNo += (back ? -1 : 1);
     // Now change to what is stored for this section in myStory
-    $scope.storySection =  $scope.myStory["section" + $scope.storySection.sectionNo]
+    $scope.storySection =  $scope.myStory["section" + $scope.storySection.sectionNo];
   }
 
   function onSectionBack(){
     onSectionForward(true);
   }
 
-
-  // Now wait for form submit buttons be clicked
-  // Needed 
-  function formSubmitClick(){
-    Story.update({_id:$scope.myStory._id},$scope.myStory);
-  }
-
-
-
-
-
-  $scope.storyData = Story();
-  
-  // form title, pic, head, text (+n)
-
-  form = {
-    title: title,
-    pic: pic+[storysection],
-    header: header+[storysection],
-    text: text+[storysection]
-  }
-
-  // $scope.submit();
-
-  $scope.submit = function (){
-
-
-    //$scope.storyData.$save(sectionsaved);
-
-
-    function sectionsaved (){
-      $scope.storysection++;
-      if ($scope.storysection > 3) {
-        console.log ("user done! storysection = ", $scope.storysection);
-      }
-    }
-  }
-
-  
-    });
-  };
 }]);
 
