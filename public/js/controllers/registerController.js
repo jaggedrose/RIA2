c2a45e3f3e26411dc74fa8312f54536a829fcdff
Sus1221 <susanne881221@hotmail.com>
2015-08-06 15:48:27 +0200

    json-file for countries-data and reset for user-reg-form

--- public/js/controllers/registerController.js
+++ public/js/controllers/registerController.js
@@ -1,2 +1,26 @@
+//"myAppName" controller.
+app.controller("registerController", ["$http", "$scope", "User", "$location", function($http, $scope, User, $location) {
+  $scope.newUser = {};
+  $scope.newUserCreate = function() {
+    $scope.createdUser = User.create($scope.newUser, function() {
+      console.log("$scope.createdUser",$scope.createdUser);
+    });
+    console.log("$scope.newUser: ", $scope.newUser);
+  };
 
-  
+  //Empties form fields
+  $scope.resetForm = function() {
+    $scope.registrationForm.$setPristine(true);
+    $scope.newUser = {};
+    console.log("$scope.registrationForm after", $scope.registrationForm);
+    //Empty newUser object
+    console.log("Reset form");
+    //Waiting for Hugos suggestion on how to remove the invalid data as well when reseting the form.
+  };
+
+  $http.get('js/resources/countries.json').then(function(res){
+    $scope.countries = res.data;
+  });
+
+}]);
+
