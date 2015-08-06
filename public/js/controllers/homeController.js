15a61fd27cd5b3ddd8e67415491d15c078b791ab
Jovan <jovan.lund@gmail.com>
2015-08-06 09:34:09 +0200

    cleaned home controller

--- public/js/controllers/homeController.js
+++ public/js/controllers/homeController.js
@@ -1,14 +1,5 @@
 //"myAppName" controller.
 app.controller("homeController", ["$http", "$scope", "Story","User", function($http, $scope, Story, User) {
-  /*$scope.newUser = User.create({
-  	user_name: "hepp",
-    first_name: "Graaaa",
-    last_name: "Grasson",
-    email: "graa@hotmail.com",
-    stad: "Malmö",
-    land: "Sverige",
-    password: "1234"
-  });*/
   $scope.newUser = User.get();
   Story.get({_populate:"user_id"}, function(data) {
     console.log("d", data);
