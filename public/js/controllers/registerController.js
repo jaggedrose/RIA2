a41cb38b01a3ca46ec7547f5466b3b55fbf6d4c6
Sus1221 <susanne881221@hotmail.com>
2015-08-07 15:17:12 +0200

    keyUp-function for email-input

--- public/js/controllers/registerController.js
+++ public/js/controllers/registerController.js
@@ -8,6 +8,10 @@ app.controller("registerController", ["$http", "$scope", "User", "$location", fu
     console.log("$scope.newUser: ", $scope.newUser);
   };
 
+  $http.get('js/resources/countries.json').then(function(res){
+    $scope.countries = res.data;
+  });
+
   //Empties form fields
   $scope.resetForm = function() {
     $scope.registrationForm.$setPristine(true);
@@ -15,12 +19,14 @@ app.controller("registerController", ["$http", "$scope", "User", "$location", fu
     console.log("$scope.registrationForm after", $scope.registrationForm);
     //Empty newUser object
     console.log("Reset form");
-    //Waiting for Hugos suggestion on how to remove the invalid data as well when reseting the form.
+    //We still need to fix the problem of invalid form data not removed when pushing reset-button
   };
 
-  $http.get('js/resources/countries.json').then(function(res){
-    $scope.countries = res.data;
-  });
+  //This is a keyUp-function for the email input-field
+  $scope.checkForEmail = function() {
+    console.log("checkForEmail function");
+    //Check if the email is already registered
+    
+  };
 
 }]);
-
