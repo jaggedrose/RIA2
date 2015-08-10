00a767d09c5563eaac2060a39ff7b989ca7cfa29
Sus1221 <susanne881221@hotmail.com>
2015-08-10 13:46:12 +0200

    error message directly when entering registered email

--- public/js/controllers/registerController.js
+++ public/js/controllers/registerController.js
@@ -6,6 +6,8 @@ app.controller("registerController", ["$http", "$scope", "User", "$location", fu
       console.log("$scope.createdUser",$scope.createdUser);
     });
     console.log("$scope.newUser: ", $scope.newUser);
+    //Add code to redirect registered user to his/her page
+
   };
 
   $http.get('js/resources/countries.json').then(function(res){
@@ -14,19 +16,46 @@ app.controller("registerController", ["$http", "$scope", "User", "$location", fu
 
   //Empties form fields
   $scope.resetForm = function() {
+    /*
     $scope.registrationForm.$setPristine(true);
     $scope.newUser = {};
     console.log("$scope.registrationForm after", $scope.registrationForm);
     //Empty newUser object
     console.log("Reset form");
     //We still need to fix the problem of invalid form data not removed when pushing reset-button
+    */
+    location.reload();
   };
 
   //This is a keyUp-function for the email input-field
-  $scope.checkForEmail = function() {
+  /*$scope.checkForEmail = function() {
     console.log("checkForEmail function");
     //Check if the email is already registered
     
-  };
+  };*/
+
+  //Every time $scope.newUser.email changes
+  $scope.$watch("newUser.email",function(newVal,oldVal){
+    $scope.emailAlreadyRegistered = false;
+    if(!newVal){return;}
+    // check against database
+    User.get({email:newVal},function(listOfUsers){
+      //If users with that email exists
+      if(listOfUsers.length){
+        // Make Error message show
+        $scope.emailAlreadyRegistered = true;
+      }
+    });
+  });
+
+  $scope.$watch("newUser.userName",function(newVal, oldVal){
+    $scope.userNameAlreadyRegistered = false;
+    if(!newVal){return;}
+    User.get({user_name:newVal},function(listOfUsers){
+      if(listOfUsers.length){
+        $scope.userNameAlreadyRegistered = true;
+      }
+    });
+  });
 
 }]);
