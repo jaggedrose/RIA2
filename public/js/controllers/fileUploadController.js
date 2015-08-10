dd09c5c52e6c0d57c4b46847f5d8061798fe9c5c
BengtAndersson <abengt@live.com>
2015-08-10 11:14:03 +0200

    Small update

---
+++ public/js/controllers/fileUploadController.js
@@ -0,0 +1,12 @@
+//File upload form controller.
+app.controller("fileUploadController", ["FileUploader", "$scope", function(FileUploader, $scope) {
+
+  $scope.formSubmit = function() {
+    console.log("files: ", $scope.files);
+    // only supporting single file upload ([0]) 
+    // at the moment...
+    FileUploader($scope.files[0]).success(function(data) {
+      console.log("saved file, public path: ", data);
+    });
+  };
+}]);
