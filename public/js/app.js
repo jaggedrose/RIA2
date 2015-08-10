3794be1f2a997526c360575a5cc8e0bb340ded16
Electromotif <pjhk@yahoo.com>
2015-08-10 13:07:58 +0200

    working on storycontroller - now we have basic save functionality of all (3) parts, user-friendly date. Pausing task awaiting image upload.

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,5 @@
 //app declaration and dependency injection
-var app = angular.module("stories", ["ngRoute", "ngResource", "ui.bootstrap"]);
+var app = angular.module("stories", ["ngRoute", "ngResource", "ngFileUpload", "ui.bootstrap"]);
 
 //app config
 app.config(["$routeProvider", "$locationProvider", function($routeProvider, $locationProvider) {
--- public/js/controllers/storyController.js
+++ public/js/controllers/storyController.js
@@ -4,10 +4,12 @@ app.controller("storyController", ["$http", "$scope", "Story", function($http, $
 
   // Create a new story and save immediately to the db
 
-  Story.create({title:""},function(arrayOfNewStories){
+  Story.create({title:"", date_created: "", date_modified: "", tags:"", number_views: ""},function(arrayOfNewStories){
     $scope.storyData = arrayOfNewStories[0];
     console.log ("created new story");
+    $scope.storyData.niceDate = niceDate ($scope.storyData.date_created);
     console.log ("$scope.storyData post created story: ", $scope.storyData);
+
   });
 
   // Counter
@@ -18,31 +20,45 @@ app.controller("storyController", ["$http", "$scope", "Story", function($http, $
     sectionNo:1, 
     header: "", 
     text: "",
-    tags: "", 
-    img: ""
+    img: "",
+    date_created: "",
+    date_modified: "",
+    number_views: "",
+
+  };
+
+  // compose a 'nice' date
+  function niceDate (date) {
+
+    var nDate = new Date (date);
+    nDate = nDate.toString();
+    nDate = nDate.slice(0,21);
+
+    return nDate;
+    
   };
     
   // On section change
   $scope.onSectionForward = function(back){
-    console.log ("back: ", back);
-    console.log ("currentSection: ", currentSection);
+    
     console.log ("$scope.storySection: ",$scope.storySection);
-    // console.log ($scope);
 
-    // Add the current section in the larger myStory object
+    // Add the current section in the larger storyData object
     $scope.storyData["section" + currentSection] = $scope.storySection;
 
     // Save to DB
     Story.update({_id:$scope.storyData._id},$scope.storyData);
 
-    // Don't do anything else if we are the last section
+    // Don't do anything else if we are in the last section
     if(
       (currentSection >= 3 && !back) ||
       (currentSection <= 1 && back)
     ){return;}
+    
     // Increment section number
     currentSection += (back ? -1 : 1);
-
+    console.log ("currentSection - post inc/dec: ", currentSection);
+    
     // Now change to what is stored for this section in myStory
     $scope.storySection =  $scope.storyData["section" + currentSection] || {};
   }
@@ -53,6 +69,7 @@ app.controller("storyController", ["$http", "$scope", "Story", function($http, $
 
   $scope.uploadImage = function(){
     console.log ("Hey! Image upload!");
+    console.log ("storyData: ", $scope.storyData);
   }
 
 
