e702eab824d19593e3c12c4b68a880222b1e8b27
Sus1221 <susanne881221@hotmail.com>
2015-08-11 13:04:05 +0200

    Function for checking for registered userNames

--- public/js/controllers/registerController.js
+++ public/js/controllers/registerController.js
@@ -1,9 +1,21 @@
 //"myAppName" controller.
 app.controller("registerController", ["$http", "$scope", "User", "$location", function($http, $scope, User, $location) {
+  
   $scope.newUser = {};
+
+  var userWhenReset = {
+    user_name: "",
+    first_name: "",
+    last_name: "",
+    email: "",
+    city: "",
+    country: "",
+    password: ""
+  };
+
   $scope.newUserCreate = function() {
-    $scope.createdUser = User.create($scope.newUser, function() {
-      console.log("$scope.createdUser",$scope.createdUser);
+    User.create($scope.newUser, function(data) {
+      console.log("user created", data);
     });
     console.log("$scope.newUser: ", $scope.newUser);
     //Add code to redirect registered user to his/her page
@@ -18,27 +30,22 @@ app.controller("registerController", ["$http", "$scope", "User", "$location", fu
   $scope.resetForm = function() {
     /*
     $scope.registrationForm.$setPristine(true);
-    $scope.newUser = {};
+    
     console.log("$scope.registrationForm after", $scope.registrationForm);
     //Empty newUser object
     console.log("Reset form");
     //We still need to fix the problem of invalid form data not removed when pushing reset-button
     */
-    location.reload();
+    /*location.reload();*/
+    console.log("Reset form! function");
+    $scope.newUser = angular.copy(userWhenReset);
   };
-
-  //This is a keyUp-function for the email input-field
-  /*$scope.checkForEmail = function() {
-    console.log("checkForEmail function");
-    //Check if the email is already registered
-    
-  };*/
 
   //Every time $scope.newUser.email changes
   $scope.$watch("newUser.email",function(newVal,oldVal){
     $scope.emailAlreadyRegistered = false;
     if(!newVal){return;}
-    // check against database
+    // check if email is registered
     User.get({email:newVal},function(listOfUsers){
       //If users with that email exists
       if(listOfUsers.length){
@@ -48,11 +55,17 @@ app.controller("registerController", ["$http", "$scope", "User", "$location", fu
     });
   });
 
-  $scope.$watch("newUser.userName",function(newVal, oldVal){
+  //Every time $scope.newUser.userName changes
+  $scope.$watch("newUser.user_name",function(newVal, oldVal){
+    console.log("$scope watch for userName", newVal, oldVal);
     $scope.userNameAlreadyRegistered = false;
     if(!newVal){return;}
+    // check if userName is registered
     User.get({user_name:newVal},function(listOfUsers){
+      console.log("listOfUsers:", listOfUsers);
       if(listOfUsers.length){
+        //Make error message show
+        console.log("Username exists");
         $scope.userNameAlreadyRegistered = true;
       }
     });
