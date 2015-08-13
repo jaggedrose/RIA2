0343953f23ab86ac5ae826cc1cbf3d4517ac6c0a
Jovan <jovan.lund@gmail.com>
2015-08-13 14:51:36 +0200

    Added filter in User.html and deleted code in userController in order to simplify the method we get the stories and post on the page

--- public/js/controllers/UserController.js
+++ public/js/controllers/UserController.js
@@ -7,11 +7,6 @@ app.controller("UserController", ["$http", "$scope", "Story","User", function($h
     
     // $scope.User = User.getById ("55c0b804b04519b813c10433") ;
     $scope.UsersStories = Story.get({user_id: $scope.User._id, _populate:"user_id"}, function() {
-      $scope.UsersStories.sort(function(x, y) {
-        var a = new Date(x.date_created);
-        var b = new Date(y.date_created);
-        return a>b ? -1 : a<b ? 1 : 0;
-      });
     });
   }) ;
 
@@ -26,7 +21,7 @@ app.controller("UserController", ["$http", "$scope", "Story","User", function($h
     User.update({_id:$scope.User._id},$scope.User);
     $location.path('/user');
   };
-  
+
   $scope.deleteStory = function(index){
     
     $scope.UsersStories[index].$remove(function(){
