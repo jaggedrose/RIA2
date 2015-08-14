3de07a889ed3ea898ca63527ff5cdc8419e3277b
Jovan <jovan.lund@gmail.com>
2015-08-14 14:19:02 +0200

    cleaned extra variable

--- public/js/controllers/ViewStoryController.js
+++ public/js/controllers/ViewStoryController.js
@@ -2,7 +2,7 @@
 app.controller("ViewStoryController", ["$http", "$scope", "$location", "Story","User","$routeParams", function($http, $scope, $location, Story, User, $routeParams) {
   var id = $routeParams.id;
   var sectionid = $routeParams.sectionid;
-  var currentSection = 1;
+  
 
   // Load the current story
   $scope.aStory = Story.getById({_id: id, _populate:"user_id"}, function(){
