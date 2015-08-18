7c2e7a32fbdadb9212bdf02dccdc8254e9afd139
tkretiv <tkretiv@gmail.com>
2015-08-18 09:58:01 +0200

    Button "Edit User profile"

--- public/js/controllers/UserController.js
+++ public/js/controllers/UserController.js
@@ -1,10 +1,10 @@
 //"myAppName" controller.
-app.controller("UserController", ["$http", "$scope", "Story","User","Login", function($http, $scope, Story, User, Login) {
+app.controller("UserController", ["$http", "$scope", "Story","User","Login",'$location', 
+  function($http, $scope, Story, User, Login,$location) {
   
 
 $scope.User=Login.user;
-hehehe = $scope.User;
-console.log("UserController", Login.user);
+// hehehe = $scope.User;
 
 function waitForUser(callback) {
   if (!$scope.User._id) {
@@ -19,16 +19,19 @@ function waitForUser(callback) {
   waitForUser(function() {
     $scope.UsersStories = Story.get({user_id: $scope.User._id, _populate:"user_id"});
     // $scope.User = User.getById ("55c0b804b04519b813c10433") ;
+    console.log("UserController :", $scope.User);
+
   });
 
   $scope.userEdit = function() {
-    $location.path('/userEdit');
-    $scope.User = Login.user;
     console.log("User ",$scope.User.userName);
+     $location.path('/userEdit');
+    // $scope.User = Login.user;
+   
   };
 
   $scope.UserChanged = function() {
-    console.log($scope.User);
+    console.log("User Changed ",$scope.User);
     User.update({_id:$scope.User._id},$scope.User);
     $location.path('/user');
   };
