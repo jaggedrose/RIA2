b7109433a7cbb42e59592036f71e6b15f8bc58db
Jovan <jovan.lund@gmail.com>
2015-08-24 15:37:10 +0200

    Functional search html and controller

--- public/js/controllers/searchController.js
+++ public/js/controllers/searchController.js
@@ -1,5 +1,5 @@
 //"myAppName" controller.
-app.controller("searchController", ["$http", "$scope", "Tag","User", function($http, $scope,Tag, User) {
+app.controller("searchController", ["$http", "$scope", "Tag","User","Story", function($http, $scope, Tag, User, Story) {
 
   $scope.show = "tags";
   
@@ -18,25 +18,42 @@ app.controller("searchController", ["$http", "$scope", "Tag","User", function($h
   $scope.search = function() {
     switch ($scope.show) {
       case 'users':
+        $scope.data="";
         User.get({user_name: new RegExp($scope.searchText, 'i')}, function(data) {
           console.log("got users", data);
           $scope.searchResults = data;
+           $scope.hashtag=("");
         });
         break;
 
       case 'tags':
+        $scope.data="";
         Tag.get({tagName: new RegExp($scope.searchText, 'i')}, function(data) {
           console.log("got tags", data);
           $scope.searchResults = data;
+          $scope.hashtag=("#");
         });
         break;
 
       default:
         Story.get({title: new RegExp($scope.searchText, 'i')}, function(data) {
-          console.log("got stories", data);
+          console.log("got stories", data.title);
           $scope.searchResults = data;
         });
     }
+  };
+
+  $scope.searchStories = function(tagid,tagName){
+    console.log("tagName",tagName);
+    var data;
+    $scope.data = Story.get({tags:tagid,_populate:"tags"});
+      console.log("data", $scope.data);    
+      $scope.searchText = ("#") + tagName ;
+      $scope.searchResults ="";
+      
+      
+
+    
   };
 
   //$scope.search();
