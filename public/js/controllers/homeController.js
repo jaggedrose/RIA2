c855c5e76cc54ca67b29497fe0fc3d36dccfb3b3
tkretiv <tkretiv@gmail.com>
2015-08-24 15:50:53 +0200

    First side 3x3 visas. Working for pagination.

--- public/js/controllers/homeController.js
+++ public/js/controllers/homeController.js
@@ -1,19 +1,54 @@
 //"myAppName" controller.
-app.controller("homeController", ["$http", "$scope","Login", function($http, $scope, Login) {
-  /*$scope.newUser = User.create({
-  	user_name: "hepp",
-    first_name: "Graaaa",
-    last_name: "Grasson",
-    email: "graa@hotmail.com",
-    stad: "Malmö",
-    land: "Sverige",
-    password: "1234"
-  });*/
+app.filter('offset', function() {
+  return function(input, start) {
+    console.log(start)
+    start = parseInt(start, 3);
 
-  // if (Login.user()) {
-  //   $scope.user = Login.user();
-  //   Story.get({user_id: $scope.user._id, _populate:"user_id"}, function(data) {
-  //     console.log("d", data);
-  //   })
-  // }
+    var result = input.slice(start);
+    return result;
+  };
+});
+
+app.controller("homeController", ["$http", "$scope", "Story","$routeParams","$location", 
+  function($http, $scope, Story, $routeParams, $location) {
+
+    $scope.AllStories = Story.get(function(){
+        console.log(" AllStories");
+    });
+
+
+    window.da = $scope;
+
+  $scope.itemsPerPage = 3;
+  $scope.currentPage = 0;
+
+   $scope.prevPage = function() {
+    if ($scope.currentPage > 0) {
+      $scope.currentPage--;
+    }
+  };
+
+  $scope.prevPageDisabled = function() {
+    return $scope.currentPage === 0 ? "disabled" : "";
+  };
+
+  $scope.pageCount = function() {
+    return Math.ceil($scope.AllStories.length/$scope.itemsPerPage)-1;
+  };
+
+  $scope.nextPage = function() {
+        console.log("Pages: ",$scope.pageCount());
+    console.log("CurrPage 1: ",$scope.currentPage);
+
+    if ($scope.currentPage < $scope.pageCount()) {
+      $scope.currentPage++;
+    }
+    console.log("CurrPage: ",$scope.currentPage);
+   
+  };
+
+  $scope.nextPageDisabled = function() {
+    return $scope.currentPage === $scope.pageCount() ? "disabled" : "";
+  };
+
 }]);
