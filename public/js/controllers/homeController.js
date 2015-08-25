b8bcf34f280cb5e8ae53621213838f350d38a64c
tkretiv <tkretiv@gmail.com>
2015-08-25 11:26:43 +0200

    First page with controller is done.

--- public/js/controllers/homeController.js
+++ public/js/controllers/homeController.js
@@ -1,54 +1,68 @@
-//"myAppName" controller.
-app.filter('offset', function() {
-  return function(input, start) {
-    console.log(start)
-    start = parseInt(start, 3);
-
-    var result = input.slice(start);
-    return result;
+app.filter('range', function() {
+  return function(input, total) {
+    
+    for (var i=1; i<=total; i++)
+      input.push(i);
+    return input;
   };
 });
 
 app.controller("homeController", ["$http", "$scope", "Story","$routeParams","$location", 
   function($http, $scope, Story, $routeParams, $location) {
 
-    $scope.AllStories = Story.get(function(){
-        console.log(" AllStories");
+    var allStories = [];
+    Story.get(function(data){
+        allStories = data.sort(function(x,y){
+          return x.date_created > y.date_created ? -1 : 1;
+        });
+        createCurrentPage(1);
+
     });
 
 
+    function createCurrentPage(page){
+      $scope.currentPageStories =  allStories.slice((page-1)*3,page*3);
+        console.log(page,$scope.currentPageStories);
+    }
+
     window.da = $scope;
 
-  $scope.itemsPerPage = 3;
-  $scope.currentPage = 0;
+  var currentPage = 1;  
 
    $scope.prevPage = function() {
-    if ($scope.currentPage > 0) {
-      $scope.currentPage--;
+     console.log("Prev: ",currentPage+" "+ $scope.pageCount());
+    if (currentPage > 1) {
+      currentPage--;
+      createCurrentPage(currentPage);
     }
   };
 
+   $scope.nextPage = function() {
+     console.log("Next: ",currentPage+" "+ $scope.pageCount());
+     if (currentPage <= $scope.pageCount()) {
+      currentPage++;
+     
+      createCurrentPage(currentPage);
+    }
+  };
+  
+  $scope.setPage = function(nPage) {
+         currentPage=nPage;
+      createCurrentPage(currentPage);
+    };
+
   $scope.prevPageDisabled = function() {
-    return $scope.currentPage === 0 ? "disabled" : "";
+    return currentPage === 1 ? "disabled" : "";
   };
 
   $scope.pageCount = function() {
-    return Math.ceil($scope.AllStories.length/$scope.itemsPerPage)-1;
+
+    return Math.ceil(allStories.length/3);
   };
 
-  $scope.nextPage = function() {
-        console.log("Pages: ",$scope.pageCount());
-    console.log("CurrPage 1: ",$scope.currentPage);
-
-    if ($scope.currentPage < $scope.pageCount()) {
-      $scope.currentPage++;
-    }
-    console.log("CurrPage: ",$scope.currentPage);
-   
-  };
 
   $scope.nextPageDisabled = function() {
-    return $scope.currentPage === $scope.pageCount() ? "disabled" : "";
+    return currentPage === $scope.pageCount() ? "disabled" : "";
   };
 
 }]);
