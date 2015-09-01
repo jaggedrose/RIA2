6915c29051922260cc343a06779db0bbcd3106bc
Electromotif <pjhk@yahoo.com>
2015-09-01 15:27:29 +0200

    show tags in viewstory, done! (viewstory.html, viewstorycontroller.js and a new directive center.js (directive needs tweaking)). also updated less and writestory.html in the process

--- public/js/controllers/ViewStoryController.js
+++ public/js/controllers/ViewStoryController.js
@@ -1,14 +1,14 @@
 //"myAppName" controller.
-app.controller("ViewStoryController", ["$http", "$scope", "$location", "Story","User","$routeParams", function($http, $scope, $location, Story, User, $routeParams) {
+app.controller("ViewStoryController", ["$http", "$scope", "$location", "$routeParams", "Story", "User", function($http, $scope, $location,  $routeParams, Story, User) {
   var id = $routeParams.id;
   var sectionid = $routeParams.sectionid;
-  
 
   // Load the current story
-  $scope.aStory = Story.getById({_id: id, _populate:"user_id"}, function(){
+  $scope.aStory = Story.getById({_id: id, _populate:"user_id tags"}, function(){
     
     // Load the current section
     $scope.storySection =  $scope.aStory["section" + sectionid];
+    console.log ("aStory: ", $scope.aStory);
     
   });
 
